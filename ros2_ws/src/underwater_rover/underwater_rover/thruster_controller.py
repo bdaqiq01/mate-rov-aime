09059484841dfe23ca5f66b36c402fa484edc879
basira <basira.daqiq0@gmail.com>
2024-08-02 16:20:14 -0700

    added ESC pins reading and methods to initialize ESC and set speed

--- ros2_ws/src/underwater_rover/underwater_rover/thruster_controller.py
+++ ros2_ws/src/underwater_rover/underwater_rover/thruster_controller.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
+import pigpio
 
 
 class ThrusterController(Node):
@@ -14,12 +15,32 @@ class ThrusterController(Node):
             10
         )
         self.subscription
+        # Initialize pigpio 
+        self.pi = pigpio.pi()
+        self.ESS_PINS = [17, 18, 27, 22, 23, 24, 25, 5] #ECS pins 
+        self.initialize_escs()
+
+    def initiazlize_escs(self):
+        for pin in self.ESS_PINS:
+            self.pi.set_servo_pulsewidth(pin, 1500)
+        self.get_logger().info('ESCs initialized neutral position')
 
     def listener_callback(self, msg):
-        self.get_logger().info('Received thruster speed: "%s" ' % str(msg.data)) 
+        self.get_logger().info('Received thruster speed: "%s" ' % str(msg.data))
+        self.set_thruster_speeds(msg.data)
 
-    def main(args = None):
-        rclpy.init(args= args)
-        thruster_controller = ThrusterController()
-        rclpy.spin(thruster_controller)
-        rclpy.shutdown()
+
+    def set_thruster_speeds(self, speeds):
+        for pin, speed in zip(self.ESS_PINS, speeds):
+            self.pi.set_servo_pulsewidth(pin, speed)
+
+def main(args = None):
+    rclpy.init(args= args)
+    thruster_controller = ThrusterController()
+    rclpy.spin(thruster_controller)
+    thruster_controller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ =='__main__':
+    main()
