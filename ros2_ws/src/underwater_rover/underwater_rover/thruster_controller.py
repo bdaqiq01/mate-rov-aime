a5c914f2fdfe54a0707e95640c469e37c832a5a9
basira <basira.daqiq0@gmail.com>
2024-08-14 16:04:42 -0700

    Added comments to thruster_controller node

--- ros2_ws/src/underwater_rover/underwater_rover/thruster_controller.py
+++ ros2_ws/src/underwater_rover/underwater_rover/thruster_controller.py
@@ -7,39 +7,39 @@ import pigpio
 
 class ThrusterController(Node):
     def __init__(self):
-        super().__init__('thruster_controller')
-        self.subscription = self.create_subscription(
-            Float32MultiArray,
-            'thruster_speeds',
-            self.listener_callback,
+        super().__init__('thruster_controller') #initialzie with the'thruster controllr' name 
+        self.subscription = self.create_subscription(  #creates a subscriptin to the thruster_speeds topic
+            Float32MultiArray,  # msg type 
+            'thruster_speeds', # name of the topic
+            self.listener_callback, # the listener-callback is called when ever a msg is received
             10
         )
         self.subscription
         # Initialize pigpio 
-        self.pi = pigpio.pi()
+        self.pi = pigpio.pi() #initialize the pigpio library for controlling GPIO pins
         self.ESS_PINS = [17, 18, 27, 22, 23, 24, 25, 5] #ECS pins 
-        self.initialize_escs()
+        self.initialize_escs() # calls the method to initalize the pins
 
     def initiazlize_escs(self):
         for pin in self.ESS_PINS:
             self.pi.set_servo_pulsewidth(pin, 1500)
-        self.get_logger().info('ESCs initialized neutral position')
+        self.get_logger().info('ESCs initialized neutral position') #logs the message indicating its state
 
-    def listener_callback(self, msg):
+    def listener_callback(self, msg): # method called whenever there is a msg received from the topib
         self.get_logger().info('Received thruster speed: "%s" ' % str(msg.data))
-        self.set_thruster_speeds(msg.data)
+        self.set_thruster_speeds(msg.data) #calls the method to update the thruster speeds based on the data
 
 
-    def set_thruster_speeds(self, speeds):
+    def set_thruster_speeds(self, speeds): # method to set the speeds
         for pin, speed in zip(self.ESS_PINS, speeds):
-            self.pi.set_servo_pulsewidth(pin, speed)
+            self.pi.set_servo_pulsewidth(pin, speed) #sets the pulse widthe for each pin to control the speed 
 
 def main(args = None):
-    rclpy.init(args= args)
-    thruster_controller = ThrusterController()
-    rclpy.spin(thruster_controller)
-    thruster_controller.destroy_node()
-    rclpy.shutdown()
+    rclpy.init(args= args) #initiats ros2 communication
+    thruster_controller = ThrusterController() #creats an instance of the node
+    rclpy.spin(thruster_controller) #keeps the node running allowing it to process callbacks
+    thruster_controller.destroy_node() #cleans up resources when the node is stopped 
+    rclpy.shutdown() #shuts down ros2 client communication
 
 
 if __name__ =='__main__':
