7ca4e1d58c9e4c7a06c1149034b513477c098569
FakeKant <thisisfake7991@gmail.com>
2024-09-18 17:58:45 -0700

    Changed data typw from Float to Float array

--- IMU_FlightBoard.py
+++ IMU_FlightBoard.py
@@ -6,12 +6,12 @@ import bluerobotics_navigator as navigator
 # See links for reference:
 # - Data message specification: http://wiki.ros.org/msg
 # - Source files for message types: https://github.com/ros/std_msgs/tree/kinetic-devel/msg
-from std_msgs.msg import Int32, Int32MultiArray
+from std_msgs.msg import Float32, Float32MultiArray
 
 # TODO: Put external library imports below
 
 
-class ExamplePublisherNode(Node):
+class AccelerationPublish(Node):
     def __init__(self):
         # TODO: Replace "example_publisher_node" with the name of the publisher node
         super().__init__('AccelerationPublish')
@@ -20,7 +20,7 @@ class ExamplePublisherNode(Node):
         # Creates a publisher attached to this node and posting to "example_topic"
         self.publisher_handle = self.create_publisher(
             # TODO: Replace "Int32" with the corresponding type of data sent over the topic
-            float32,
+            Float32MultiArray,
             
             # TODO: Replace "example_topic" with the name of the topic
             'Acceleration',
@@ -40,16 +40,14 @@ class ExamplePublisherNode(Node):
     """
     def publish_data_onto_topic(self):
         # Create the message packet
-        #msg = Int32()
-        #msg.data = 42
-
+        
         #Acceleration - Float value
-        msg = float32()
-        mgs.data = 42
+        #read acceleration data in X,Y,Z directions
+        acceleration = navigator.read_accel()
+        msg = Float32()
+        mgs.data = float(acceleration)
 
         #Angular Velocity - float value
-
-        #
 
         # Publish it on the topic "example_topic"
         self.publisher_handle.publish(msg)
@@ -65,7 +63,18 @@ def main(args=None):
     # Start up the node
     i = 1
     while i = 1:
-        print(1)
+        acceleration = navigator.read_accel()
+        
+        #Forward is +X direction. Backward is -X direction
+        forwardAcceleration = acceleration.x
+        
+        #Left is +Y direction. Right is -Y direction 
+        sideAcceleration = acceleration.y
+        
+        #Up is +Z direction. Down is -Z direction
+        verticalAcceleration = acceleration.z
+        
+        print("-1-")
     else:
 
     # NOTE: This line will block until a shutdown command is received (like Ctrl + C)
