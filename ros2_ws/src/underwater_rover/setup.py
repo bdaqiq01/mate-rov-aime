4884c90175b7eff4860e70273a34f682867f5726
tjkhan2006 <tahajkhan8475@gmail.com>
2024-10-04 17:57:25 -0700

    Added all relevent nodes to setup.py

--- ros2_ws/src/underwater_rover/setup.py
+++ ros2_ws/src/underwater_rover/setup.py
@@ -20,7 +20,13 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "test_node = underwater_rover.test_node:main"
+            "test_node = underwater_rover.test_node:main",
+            "temperature_node = underwater_rover.temperature_sensor:main",
+            "thruster_node = underwater_rover.thruster_controller:main",
+            "claw_1_node = underwater_rover.servo_claw_1:main",
+            "navigator_reader_node = underwater_rover.navigator_reader:main",
+            "draw_circle_node = underwater_rover.draw_circle:main",
+            "barometer_node = underwater_rover.barometer_node:main",
         ],
     },
 )
