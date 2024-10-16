8c3993f8b697f0d698cf2883d8e5a9acdf5e6587
Jesus <jesusretes60@gmail.com>
2024-10-15 17:36:42 -0700

    some changes to the commmeted committed

---
+++ non_ros_test_code/servoclawLeft_Tests.py
@@ -0,0 +1,38 @@
+#The following is the original logic used to create the Subscriber Node
+
+ #Variables
+
+ x_button = bool()
+ x_check = False
+ servo_state = "Open"
+ #These two variables are the open and closed state of the servo 
+ servo_open = 0
+ servo_close = 180
+ #This is used to store the current value of the signal pin the servo is connected to
+ pwm = int()
+ #The folllowing two variables are the pwm ranges for the servo and it should only be two numbers
+ pwm_range = [500,1200]
+ #This sets displays the current pos. of the servo in degrees
+ current_angle = int()
+
+ #Functions
+
+
+ #Changes the servo position to an angle in degrees you must use an integer
+
+   current_angle = map_range(pwm,500,2500,0,180)
+
+ # This checks the state of the x_button and the changes the position of the servo claw accordingly 
+ while True:
+   if x_button == False & x_check == True:
+     servo_state = servo_state
+   if x_button == False & x_check == False:
+      servo_state = servo_state 
+   if x_button == True & x_check == True:
+     servo_state = "closed"
+     x_check = False
+     print("closed")
+   if x_button == True & x_check == False:
+     servo_state = "open"
+     x_check = True
+     print("open")
